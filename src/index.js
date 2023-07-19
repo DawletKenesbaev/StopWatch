c6fd6a1ccad921a723ae94e26c4fa24d5d174b57
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-07-19 12:19:18 +0500

    lllol

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Stopwatch from './StopWatch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Stopwatch />
   </React.StrictMode>
 );
 
